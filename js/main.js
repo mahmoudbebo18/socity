44a5f47572f1f32be5e55d194bee2e5c23523f8f
mahmoudElkhateb18 <m.elkhateb@exp-sa.com>
2022-06-25 00:46:22 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,59 @@
+$(document).ready(function(){
+
+
+    $('.experiance ul li').click(function(){
+        $('.experiance ul li').removeClass('active')
+        $(this).addClass('active')
+        
+    }),
+    
+    $('.certificate ul li').click(function(){
+        $('.certificate ul li').removeClass('active')
+        $(this).addClass('active')
+        
+    });
+
+    $('.major ul li').click(function(){
+        $('.major ul li').removeClass('active')
+        $(this).addClass('active')
+        
+    })
+
+    $('.first-row .number li').click(function(){
+        $('.first-row .number li').removeClass('active')
+        $(this).addClass('active')
+        
+    });
+    $('.sec-row .number li').click(function(){
+        $('.sec-row .number li').removeClass('active')
+        $(this).addClass('active')
+        
+    })
+    $('.third-row .number li').click(function(){
+        $('.third-row .number li').removeClass('active')
+        $(this).addClass('active')
+        
+    })
+    $(' .fourth-row .number li').click(function(){
+        $('.fourth-row .number li').removeClass('active')
+        $(this).addClass('active')
+        
+    })
+    $('.fifth-row  .number li').click(function(){
+        $('.fifth-row .number li').removeClass('active')
+        $(this).addClass('active')
+        
+    })
+    $('.sixth-row .number li').click(function(){
+        $('.sixth-row .number li').removeClass('active')
+        $(this).addClass('active')
+        
+    })
+    $('.seventh-row .number li').click(function(){
+        $('.seventh-row .number li').removeClass('active')
+        $(this).addClass('active')
+        
+    })
+
+
+})
